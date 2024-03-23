c816b0a5c224ed3298a945370ce5a3e6307b186e
eventhorizonsky <eventhorizonsky@gmail.com>
2024-03-23 23:24:58 +0800

    迁移数据库改用sqlite

--- init_db.sh
+++ init_db.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-DB_FILE="/app/data/mydatabase.db"
+DB_FILE="/mydatabase.db"
 
 if [ ! -f "$DB_FILE" ]; then
     echo "Initializing database..."
     # 在这里执行初始化数据库的命令，例如：
-    sqlite3 "$DB_FILE" 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT);'
+    sqlite3 "$DB_FILE" 'CREATE TABLE IF NOT EXISTS video_metadata (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            file_path TEXT,
+                            file_name TEXT,
+                            file_size INTEGER,
+                            hash_value TEXT,
+                            file_extension TEXT,
+                            episode_id INTEGER,
+                            anime_id INTEGER,
+                            anime_title TEXT,
+                            episode_title TEXT,
+                            type TEXT,
+                            type_description TEXT,
+                            shift INTEGER,
+                            matched TEXT
+                        );'
     echo "Database initialized."
 else
     echo "Database file already exists. Skipping initialization."
