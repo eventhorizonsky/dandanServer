fdc765546251981b0710ef380400351cbd1fcd59
eventhorizonsky <eventhorizonsky@gmail.com>
2024-04-06 18:43:33 +0800

    get代理方法修改

--- dandanWeb-rest/src/main/java/xyz/ezsky/controller/ProxyController.java
+++ dandanWeb-rest/src/main/java/xyz/ezsky/controller/ProxyController.java
@@ -1,10 +1,14 @@
 package xyz.ezsky.controller;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/dandanapi/api/v2")
@@ -17,10 +21,30 @@ public class ProxyController {
     }
 
     @GetMapping("/**")
-    public Object  getRequest(HttpServletRequest request) {
-        String externalUrl = "https://api.dandanplay.net" + request.getRequestURI().split("/dandanapi")[1];
-        return restTemplate.getForObject(externalUrl, Object.class);
+    public Object getRequest(HttpServletRequest request) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        String externalUrl = "https://api.dandanplay.net" + request.getRequestURI().substring("/dandanapi".length());
+        String queryString = request.getQueryString();
+        if (queryString != null) {
+            externalUrl = externalUrl + "?" + queryString;
+        }
+
+        Request httpRequest = new Request.Builder()
+                .url(externalUrl)
+                .build();
+
+        try (Response response = client.newCall(httpRequest).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected response code: " + response);
+            }
+
+            // 这里可以根据需要处理响应，比如将响应转换为对象等
+            String responseBody = response.body().string();
+            return responseBody;
+        }
     }
+
     @PostMapping("/**")
     public ResponseEntity<Object> postRequest(HttpServletRequest request, @RequestBody(required = false) Object requestBody) {
         String externalUrl = "https://api.dandanplay.net" + request.getRequestURI().split("/dandanapi")[1];
@@ -30,6 +54,6 @@ public class ProxyController {
 
         HttpEntity<Object> requestEntity = new HttpEntity<>(requestBody, headers);
 
-        return restTemplate.postForEntity(externalUrl, requestEntity, Object.class);
+            return restTemplate.postForEntity(externalUrl, requestEntity, Object.class);
     }
 }
