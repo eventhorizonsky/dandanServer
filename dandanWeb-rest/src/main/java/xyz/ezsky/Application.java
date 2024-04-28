4b6f705eded6e56469c28865066d4da38d26e991
eventhorizonsky <eventhorizonsky@gmail.com>
2024-04-28 22:35:04 +0800

    去除媒体文件夹的指定 为mybatis设置扫描路径

--- dandanWeb-rest/src/main/java/xyz/ezsky/Application.java
+++ dandanWeb-rest/src/main/java/xyz/ezsky/Application.java
@@ -1,5 +1,6 @@
 package xyz.ezsky;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@MapperScan("xyz.ezsky.dao")
 public class Application {
 
     public static void main(String[] args) {
