435f1dfca5ed636caca12dd8852b50d1d52fd5b7
eventhorizonsky <eventhorizonsky@gmail.com>
2024-10-08 22:25:27 +0800

    对wasm的mineType做处理

--- dandanWeb-rest/src/main/java/xyz/ezsky/config/WebConfig.java
+++ dandanWeb-rest/src/main/java/xyz/ezsky/config/WebConfig.java
@@ -2,6 +2,7 @@ package xyz.ezsky.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.ezsky.interceptor.CustomInterceptor;
@@ -16,5 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器并指定拦截路径
         registry.addInterceptor(customInterceptor).addPathPatterns("/**");
+    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.mediaType("wasm", org.springframework.http.MediaType.valueOf("application/wasm"));
     }
 }
